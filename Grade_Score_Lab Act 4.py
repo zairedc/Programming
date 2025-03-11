5029b85c5a35b38a95cc615e8bfe0508e66de4ba
Jeniah Zaire Dela Cruz <jeniahzairedc@gmail.com>
2025-03-11 23:34:08 +0800

    Create Grade_Score_Lab Act 4.py
    By entering the student's score, it will determine the grade of it.

---
+++ Grade_Score_Lab Act 4.py
@@ -0,0 +1,17 @@
+def assign_grade(score):
+    if score < 0 or score > 100:
+        print("Invalid score! Please enter a value between 0 and 100.")
+    elif score >= 90:
+        print("Grade: A")
+    elif score >= 80:
+        print("Grade: B")
+    elif score >= 70:
+        print("Grade: C")
+    elif score >= 60:
+        print("Grade: D")
+    else:
+        print("Grade: F")
+
+#Input here
+user_input = int(input("Enter student's score here: "))
+assign_grade(user_input)
